3ad6fc295ed55a46f863ebfc2175bb1de011a0c7
Ednilton Matos <edniltonprogramador@gmail.com>
2023-01-21 20:25:25 -0300

    books: docs: aula #09 finalizada nessa aula foi ensinado os conceitos truthy e falsy

---
+++ javascript/Curso-javascript/JavaScript #09 - Conceito de Truthy & Falsy/main.js
@@ -0,0 +1,42 @@
+// TRUTHY E FALSY
+
+// !!X = checa se é true ou false
+// string vazia "" = false
+
+const stringVazia = ""
+
+console.log(!!stringVazia)
+
+// 0 = false
+
+const zero = 0
+
+console.log(!!zero)
+
+// undefined = false
+
+const undefinede = undefined
+
+console.log(!!undefinede)
+
+// null = false
+
+const testeNull = null
+
+console.log(!!testeNull)
+
+// array vazio [] = true
+
+const arrayVazio = []
+
+console.log(!!arrayVazio)
+
+// objeto vazio {} = true
+
+const objetoVazio = {}
+
+console.log(!!objetoVazio)
+
+// !variavel = inverte o booleano
+
+console.log(!objetoVazio)
